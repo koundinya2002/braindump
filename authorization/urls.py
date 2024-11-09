0524fbedc32b95302294ab20aab6623595cda235
Koundinya <123861193+koundinya2002@users.noreply.github.com>
2024-11-09 08:37:51 +0530

    renew files

--- authorization/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import UserRegisterView
-
-urlpatterns = [
-    path('', views.landing, name="landing"),
-    path('register/', UserRegisterView.as_view(), name="register"),
-]
--- thoughts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Thoughts
-
-admin.site.register(Thoughts)
